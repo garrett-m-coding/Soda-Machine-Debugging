ef749f1cb2ffcba4d8b688c335a1040bb837ad49
garrett-m-coding <garrett.mcginnis.coding@gmail.com>
2021-09-15 11:14:20 -0500

    Within the wallet class I imported coins. Within the user_interface file I changed i++ to i+1. Within the simulation class I imported user_interface and changed the single = sign to a == sign. Within the customer class I imported wallet, backpack, and user_interface. I also updated the check_backpack method by replacing backpack with self so that it reads self.purchased_cans... I also fixed the add_coins_to_wallet method to have a coins_list rather than a coin_list.

--- soda_machine.py
+++ soda_machine.py
@@ -9,13 +9,13 @@ class SodaMachine:
     def fill_register(self):
         """Method will fill SodaMachine's register with certain amounts of each coin when called."""
         for index in range(8):
-        self.register.append(coins.Quarter())
+            self.register.append(coins.Quarter())
         for index in range(10):
-        self.register.append(coins.Dime())
+            self.register.append(coins.Dime())
         for index in range(20):
-        self.register.append(coins.Nickel())
+            self.register.append(coins.Nickel())
         for index in range(50):
-        self.register.append(coins.Penny())
+            self.register.append(coins.Penny())
 
     def fill_inventory(self):
         """Method will fill SodaMachine's cans list with certain amounts of each can when called."""
@@ -67,7 +67,7 @@ class SodaMachine:
             customer.add_coins_to_wallet(customer_payment)
             self.return_inventory(selected_soda)
 
-    def gather_change_from_register(self, change_value)
+    def gather_change_from_register(self, change_value):
         change_list = []
         while change_value > 0:
             if change_value >= 0.25 and self.register_has_coin("quarter"):
@@ -129,7 +129,7 @@ class SodaMachine:
         """Re-adds a remove can back to inventory upon unsuccessful purchase attempt"""
         self.inventory.append(chosen_soda)
 
-    def deposit_coins_into_register(self, coin_list):
+    def deposit_coins_into_register(self, coins_list):
         """Takes in list of coins as argument, adds each coin from list to the register"""
         for coin in coins_list:
             self.register.append(coins_list)
