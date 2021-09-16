21e58650de8331dec269bb1e80edf92df8baf196
garrett-m-coding <garrett.mcginnis.coding@gmail.com>
2021-09-16 14:04:00 -0500

    Within the customer class and within gather_coins_from_wallet method I changed will_proceed from false to true. Within the user_interface file and within the display_can_cost method I changed the selected_can_price to selected_can_name. Within the same user_interface file within the display_payment_value method I changed the for loop to add coin.value rather than 1 for correct total_payment_value. Within the coin_selection method changed what the user needs to enter so that it matches the switcher.

--- user_interface.py
+++ user_interface.py
@@ -113,14 +113,14 @@ def get_unique_can_names(inventory):
 
 def display_can_cost(selected_can):
     """Displays the name of a can and its price"""
-    print(f'The price of a {selected_can.price} is ${selected_can.price}')
+    print(f'The price of a {selected_can.name} is ${selected_can.price}')
 
 
 def display_payment_value(customer_payment):
     """Displays the value of selected coins as customer is choosing coins to deposit"""
     total_payment_value = 0
     for coin in customer_payment:
-        total_payment_value += 1
+        total_payment_value += coin.value
     total_payment_value = round(total_payment_value, 2)
     print(f'You currently have ${total_payment_value} in hand')
 
@@ -129,10 +129,10 @@ def coin_selection():
     """Prompts user to choose which coins to deposit and passes their selection in validate_coin_selection"""
     validated_user_selection = (False, None)
     while validated_user_selection[0] is False:
-        print("\n\tEnter -Q- for Quarter")
-        print("\tEnter -D- for Dime")
-        print("\tEnter -N- for Nickel")
-        print("\tEnter -P- for Penny")
+        print("\n\tEnter -1- for Quarter")
+        print("\tEnter -2- for Dime")
+        print("\tEnter -3- for Nickel")
+        print("\tEnter -4- for Penny")
         print("\tEnter -5- for when finished to deposit payment into machine")
         user_input = try_parse_int(input())
         validated_user_selection = validate_coin_selection(user_input)
